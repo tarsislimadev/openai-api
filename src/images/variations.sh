95cae3ab5b7d9d4907de25e2be9691d7cf0a5c1f
Tarsis <br.tmvdl@gmail.com>
2024-02-27 18:07:51 -0300

    Updating app files

---
+++ src/images/variations.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+. .env
+
+PATH="completions"
+
+FILENAME="${1}"
+
+/usr/bin/curl  https://api.openai.com/v1/images/variations \
+  --header "Authorization: Bearer ${API_VERSION}" \
+  -F image="@${FILENAME}" \
+  -F n=2 \
+  -F size="1024x1024"
